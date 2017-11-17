84188e88b7ad98ebbb5629c9dd08124b8fa7bc09
adrianperez <adrian.perez.456@gmail.com>
2017-11-17 15:56:30 -0600

    Created Repositories for threads and posts

--- src/main/java/live/jointheconversation/demo/repositories/PostRepository.java
+++ src/main/java/live/jointheconversation/demo/repositories/PostRepository.java
@@ -2,7 +2,9 @@ package live.jointheconversation.demo.repositories;
 
 import live.jointheconversation.demo.models.Post;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends CrudRepository<Post,Long>{
     Post findByTitle(String title);
 }
--- src/main/java/live/jointheconversation/demo/repositories/ThreadRepository.java
+++ src/main/java/live/jointheconversation/demo/repositories/ThreadRepository.java
@@ -1,7 +1,11 @@
 package live.jointheconversation.demo.repositories;
 
+import live.jointheconversation.demo.models.Thread;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ThreadRepository extends CrudRepository<Thread, Long>{
     Thread findByTitle(String title);
+    Thread save(Thread thread);
 }
