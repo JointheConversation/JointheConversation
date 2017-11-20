2d955113a4a561cc246664770886393cf30f0e53
becca <r.alexis.tolentino@gmail.com>
2017-11-20 12:21:58 -0600

    make build config

--- src/main/java/live/jointheconversation/demo/controllers/LandingPage.java
+++ src/main/java/live/jointheconversation/demo/controllers/LandingPage.java
@@ -1,15 +1,17 @@
 package live.jointheconversation.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LandingPage {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String landingPage(){
-        return "Here is the landing page";
+    @GetMapping("/index")
+    public String landingPage( Model viewModel ){
+        viewModel.addAttribute("Converse", null);
+        return "index";
     }
 }
--- src/main/java/live/jointheconversation/demo/models/Thread.java
+++ src/main/java/live/jointheconversation/demo/models/Thread.java
@@ -1,7 +1,5 @@
 package live.jointheconversation.demo.models;
 
-import com.sun.tools.javah.Gen;
-
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
