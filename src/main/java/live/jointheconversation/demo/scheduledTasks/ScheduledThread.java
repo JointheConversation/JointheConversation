f5f2923b9602af4093f7ca04916a1c07d20474e1
adrianperez <adrian.perez.456@gmail.com>
2017-11-20 14:52:39 -0600

    Created the scheduled task for closing threads after a set amount of time.

--- src/main/java/live/jointheconversation/demo/scheduledTasks/ScheduledThread.java
+++ src/main/java/live/jointheconversation/demo/scheduledTasks/ScheduledThread.java
@@ -1,4 +1,37 @@
 package live.jointheconversation.demo.scheduledTasks;
 
+import live.jointheconversation.demo.services.ThreadService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
 public class ScheduledThread {
+    private static final Logger log = LoggerFactory.getLogger(ScheduledThread.class);
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    private Thread thread;
+    private ThreadService threadService;
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void updateThreads(Thread thread, ThreadService threadService){
+        this.thread=thread;
+        this.threadService=threadService;
+
+        for(threadService.findAll())
+        if(thread.get){
+
+        }
+
+
+    }
 }
