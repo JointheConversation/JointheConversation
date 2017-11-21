1def0335117fb4bf003bdeb23ff3f297852c6d43
adrianperez <adrian.perez.456@gmail.com>
2017-11-21 15:52:56 -0600

    Added comments to the code.

--- src/main/java/live/jointheconversation/demo/services/ThreadCountService.java
+++ src/main/java/live/jointheconversation/demo/services/ThreadCountService.java
@@ -17,12 +17,16 @@ public class ThreadCountService {
         this.threadDao=threadDao;
     }
         public void compare(List<ThreadCount> threadCounts) {
+        //This code sorts through the list based on the thread count and orders them from high count to low count
+            //will need to refactor later in the event of a tie.
         threadCounts.sort(Comparator.comparingLong(ThreadCount::getCount).reversed());
+        //THis for each loop checks to make sure hta the sort worked correctly and that the information is beind dispalyed from high thread count to low
             for (ThreadCount threadcount:threadCounts
                  ) {
                 System.out.println("The title is "
                         + threadcount.getThreadTitle());
                 System.out.println("The thread count is "+threadcount.getCount());
+                //This is there We will call up on the ThreadWInner model in order to set the winner into that table.
 
             }
 
