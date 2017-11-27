2bb40b60cfb3ad59e97988e3165e0392f0a5ec37
becca <r.alexis.tolentino@gmail.com>
2017-11-27 14:45:05 -0600

    Trying to debug problems

--- src/main/java/live/jointheconversation/demo/repositories/UserRepository.java
+++ src/main/java/live/jointheconversation/demo/repositories/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
     public User findByUsername(String username);
-    public User findByEmail(String email);
+    public User findByEmail(String email);}
 
