f4811aac92bfd9e7752c775dd9db3d8ccf367813
adrianperez <adrian.perez.456@gmail.com>
2017-11-29 19:01:21 -0600

    Turned off scheduling for the moment

--- src/main/java/live/jointheconversation/demo/scheduledTasks/ScheduledThread.java
+++ src/main/java/live/jointheconversation/demo/scheduledTasks/ScheduledThread.java
@@ -42,7 +42,7 @@ public class ScheduledThread {
 ////        log.info("30 seconds have passed, the time is now {}", dateFormat.format(new Date()));
 //
 //    }
-    @Scheduled(fixedRate = 300000) //  6 minutes 
+//    @Scheduled(fixedRate = 360000) //  6 minutes
     public void countThreadPosts(){
         //Sets all of the winner posts to in-active so that only the current winner will be displayed.
         Iterable<ThreadWinner> threadsWinnerList=threadWinnerDao.findAll();
