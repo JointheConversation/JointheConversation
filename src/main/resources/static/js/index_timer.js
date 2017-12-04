2e62f71371621156004fbc207a281c5a4fd0b322
becca <r.alexis.tolentino@gmail.com>
2017-12-04 14:17:21 -0600

    custom css

--- src/main/resources/static/js/index_timer.js
+++ src/main/resources/static/js/index_timer.js
@@ -1,38 +1,80 @@
+// "use strict";
+// var date;
+// var display = document.getElementById('time');
+//
+// $(document).ready(function() {
+//     getTime('GMT', function(time){
+//         date = new Date(time);
+//     });
+// });
+//
+// setInterval(function() {
+//     date = new Date(date.getTime() + 1000);
+//
+//     var currenthours = date.getHours();
+//     var currentSecs = date.getSeconds();
+//     var hours;
+//     var minutes;
+//     var seconds;
+//     if (currenthours == 23 && currentsecs == 0){
+//         display.innerHTML = 'LIVE NOW';
+//     } else {
+//         if (currenthours < 23) {
+//             hours = 22 - currenthours;
+//         } else {
+//             hours = 23;
+//         }
+//         minutes = 60 - date.getMinutes();
+//         seconds = 60 - date.getSeconds();
+//
+//         if (minutes < 10) {
+//             minutes = '0' + minutes;
+//         }
+//         if (seconds < 10) {
+//             seconds = '0' + seconds;
+//         }
+//         display.innerHTML = hours + ':' + minutes + ':' +seconds;
+//     }
+// }, 1000);
+
+
+
 "use strict";
-var date;
-var display = document.getElementById('time');
 
-$(document).ready(function() {
-    getTime('GMT', function(time){
-        date = new Date(time);
-    });
-});
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    return {
+        'total': t,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
 
-setInterval(function() {
-    date = new Date(date.getTime() + 1000);
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
 
-    var currenthours = date.getHours();
-    var currentSecs = date.getSeconds();
-    var hours;
-    var minutes;
-    var seconds;
-    if (currenthours == 23 && currentsecs == 0){
-        display.innerHTML = 'LIVE NOW';
-    } else {
-        if (currenthours < 23) {
-            hours = 22 - currenthours;
-        } else {
-            hours = 23;
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
         }
-        minutes = 60 - date.getMinutes();
-        seconds = 60 - date.getSeconds();
-
-        if (minutes < 10) {
-            minutes = '0' + minutes;
-        }
-        if (seconds < 10) {
-            seconds = '0' + seconds;
-        }
-        display.innerHTML = hours + ':' + minutes + ':' +seconds;
     }
-}, 1000);
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
