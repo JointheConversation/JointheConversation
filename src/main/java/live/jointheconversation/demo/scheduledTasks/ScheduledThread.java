449931c689b36815a6122b55c6dfd6ec1e0559c6
adrianperez <adrian.perez.456@gmail.com>
2017-12-05 14:42:36 -0600

    Set timer back moved about use to profile page.

--- src/main/java/live/jointheconversation/demo/scheduledTasks/ScheduledThread.java
+++ src/main/java/live/jointheconversation/demo/scheduledTasks/ScheduledThread.java
@@ -44,7 +44,7 @@ public class ScheduledThread {
 //    }
 //    @Scheduled(fixedRate = 360000) //  6 minutes
     //Change back to 10 in the morning
-    @Scheduled(cron = "0 33 12 ? * *")
+    @Scheduled(cron = "0 0 10 * * *")
     public void countThreadPosts(){
         //Sets all of the winner posts to in-active so that only the current winner will be displayed.
         Iterable<ThreadWinner> threadsWinnerList=threadWinnerDao.findAll();
